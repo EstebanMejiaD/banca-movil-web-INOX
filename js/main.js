27b0b5925863f3b0ee153766f9ce139c6639dd0f
Esteban Mejia <estebandeveloper10@gmail.com>
2022-09-20 15:53:56 -0500

    Primera intervencion del diseño UI de la pagina principal

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(window).on("scroll", function() {
+
+    if ($(".navbar").offset().top > 40) {
+       $(".navbar").addClass("navbar-fixed");
+       $(".go-top").show();
+ 
+    } else {
+       $(".navbar").removeClass("navbar-fixed");
+       $(".go-top").hide();
+ 
+    }
+ })
+ 
+ $(document).ready(function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+       itemSelector: '.portfolio-item'
+    });
+ 
+    $('#portfolio-filters li').on('click', function() {
+       $("#portfolio-filters li").removeClass('filter-active');
+       $(this).addClass('filter-active');
+ 
+       portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+       });
+    });
+ 
+    $('.popup-image').magnificPopup({
+       type: 'image',
+       closeOnContentClick: true,
+       gallery: {
+          enabled: true,
+          navigateByImgClick: true
+       }
+    });
+ 
+    $("#navbarNav").on('show.bs.collapse', function() {
+ 
+       $(".navbar").addClass("navbar-fixed");
+ 
+       $('a.nav-link, a.btn-custom').click(function() {
+          $("#navbarNav").collapse('hide');
+       });
+    });
+ 
+ 
+ });
